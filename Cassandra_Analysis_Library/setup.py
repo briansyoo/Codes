bfed71eae85a77fd4f6078bfd7a9a3fa5877db04
Brian <briansyoo@gmail.com>
2018-01-06 17:08:45 -0500

    updated setup.py

--- Cassandra_Analysis_Library/setup.py
+++ Cassandra_Analysis_Library/setup.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
 
+from setuptools import setup
+
 if sys.argv[-1] == 'readme':
     print(readme)
     sys.exit()
 
-m setuptools import setup
 
 setup(name='funniest',
-      version='0.1',
-      description='Cassandra Analysis Library (CAL) for post analysis of CASSANDRA output files.',
-      url='http://github.com/storborg/funniest',
-      author='Brian Yoo',
-      author_email='briansyoo@gmail.com',
-      license='MIT',
-      packages=['funniest'],
-      install_requires=[
-          'numpy',
-      ],
-      zip_safe=False)
+	version='0.1',
+	description='Cassandra Analysis Library (CAL) for post analysis of CASSANDRA output files.',
+	url='https://github.com/briansyoo/Codes',
+	author='Brian Yoo',
+	author_email='briansyoo@gmail.com',
+	license='MIT',
+	scripts=['bin/cal_angle.py','bin/cal_rdf.py','bin/cal_angle.py','bin/cal_dihedral.py'],
+	install_requires=[
+	'numpy','matplotlib',
+	  ],
+	  zip_safe=False)
 
 
