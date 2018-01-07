8cf9137c25ff1582dbab9504cc990f3e8349f1a2
Brian <briansyoo@gmail.com>
2018-01-06 19:07:48 -0500

    fixed readme flag

--- Cassandra_Analysis_Library/setup.py
+++ Cassandra_Analysis_Library/setup.py
@@ -2,6 +2,9 @@
 import sys
 from setuptools import setup
 
+with open('README.md') as f:
+	readme = f.read()
+
 if sys.argv[-1] == 'readme':
     print(readme)
     sys.exit()
@@ -15,6 +18,10 @@ setup(name='CassandraAnalysisLibrary',
 	author_email='briansyoo@gmail.com',
 	license='MIT',
 	scripts=['bin/cal_angle.py','bin/cal_rdf.py','bin/cal_angle.py','bin/cal_dihedral.py','bin/cal_plot.py'],
+	classifiers=[
+		'Programming Language : Python :: 2.6',
+		'Programming Language : Python :: 2.7',
+	],
 	install_requires=[
 	'numpy','matplotlib',
 	  ],
